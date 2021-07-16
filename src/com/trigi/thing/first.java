524840b3e5b46e392f1475fe0ddcff99c01d911f
trigi1 <trigintillion1@gmail.com>
2021-07-16 12:43:59 -0700

    random number of random numbers

--- src/com/trigi/thing/first.java
+++ src/com/trigi/thing/first.java
@@ -6,9 +6,15 @@ public class first {
         return (int) Math.floor(Math.random() * (y-x+1) + x);
     }
     public static void main(String[] args) {
-        System.out.println("5 random numbers from 1 to 10:");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(rNumber(1,10));
+        int x = rNumber(1, 10);
+        int y = rNumber(1, 10);
+        if (x == 1) {
+            System.out.println(x + " random number from 1 to " + y + ":");
+        } else {
+            System.out.println(x + " random numbers from 1 to " + y + ":");
+        }
+        for (int i = 0; i < x; i++) {
+            System.out.println(rNumber(1,y));
         }
     }
 }
