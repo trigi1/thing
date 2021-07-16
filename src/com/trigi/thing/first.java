4e6ebfe4c6a53da59e1ab2b68fa0cc5f8b095840
trigi1 <trigintillion1@gmail.com>
2021-07-16 10:25:12 -0700

    epic 5 random numbers.

--- src/com/trigi/thing/first.java
+++ src/com/trigi/thing/first.java
@@ -1,7 +1,10 @@
 package com.trigi.thing;
-
+import java.lang.Math;
 public class first {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("5 random numbers from 1 to 10:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println((int) Math.floor(Math.random() * 10 + 1));
+        }
     }
 }
