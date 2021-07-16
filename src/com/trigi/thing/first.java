59a0f491d5ce459cf073f57e0c3a698c2413f1ab
trigi1 <trigintillion1@gmail.com>
2021-07-16 10:34:48 -0700

    epic functions.

--- src/com/trigi/thing/first.java
+++ src/com/trigi/thing/first.java
@@ -1,10 +1,14 @@
 package com.trigi.thing;
 import java.lang.Math;
 public class first {
+    // rNumber(x, y) returns an integer from x to y, inclusive.
+    public static int rNumber(int x, int y) {
+        return (int) Math.floor(Math.random() * (y-x+1) + x);
+    }
     public static void main(String[] args) {
         System.out.println("5 random numbers from 1 to 10:");
         for (int i = 0; i < 5; i++) {
-            System.out.println((int) Math.floor(Math.random() * 10 + 1));
+            System.out.println(rNumber(1,10));
         }
     }
 }
